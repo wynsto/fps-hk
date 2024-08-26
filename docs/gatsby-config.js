a7e75d200ce883a9ea7351395f7085e9bea07dc3
Wynston Hou <houyewei@gmail.com>
2024-08-26 10:41:22 +0800

    new docs

---
+++ docs/gatsby-config.js
@@ -0,0 +1,35 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  pathPrefix: "/fps-hk",
+  siteMetadata: {
+    title: `Faster Payment System QR Code Generator`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{ 
+    resolve: "gatsby-plugin-google-gtag",
+    options: {
+      "trackingIds": ["G-6TLC8058Y5"]
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, "gatsby-plugin-mdx", "gatsby-transformer-remark", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }, {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  }]
+};
