fcc17ec806cb5769a5c512ea8a933e93d40a9fa6
Wynston <houyewei@gmail.com>
2025-09-27 10:05:59 +0800

    fix issue

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -9,23 +9,8 @@ const pageStyles = {
   padding: 96,
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
 }
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
 const paragraphStyles = {
   marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
 }
 const listStyles = {
   marginBottom: 96,
