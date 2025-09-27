7e9fc3285edd9ee0a8e711a03fb99eafd4f1f0de
Wynston <houyewei@gmail.com>
2025-09-27 09:26:54 +0800

    ssr flag

--- docs/gatsby-config.ts
+++ docs/gatsby-config.ts
@@ -1,9 +1,9 @@
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
-  flags: {
-    DEV_SSR: true,
-  },
+  // flags: {
+  //   DEV_SSR: true,
+  // },
   siteMetadata: {
     title: `docs2`,
     siteUrl: `https://www.yourdomain.tld`
