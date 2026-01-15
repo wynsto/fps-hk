bae7f247cbd9ff5e09e07ee1a76247c8d1bd21d1
Wynston Hou <4262443+wynsto@users.noreply.github.com>
2026-01-15 13:00:12 +0800

    fix ui

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -226,14 +226,18 @@ const IndexPage = () => {
           <input placeholder="銀行編號 Bank Code" value={bankCode} onChange={onBankCodeChange} />
         </div>
       }
-      <input className="inline-block" placeholder={"Please input bill ref No"} value={billRefNo} onChange={onBillRefNoChange} />
-      <select onChange={onExtensionChange} value={fileExt}>
-        <option value="png">PNG</option>
-        <option value="jpeg">JPEG</option>
-        <option value="webp">WEBP</option>
-      </select>
-      <button onClick={onDownloadClick}>Download</button>
-
+      <div>
+        <input className="inline-block" placeholder={"帳單編號 Bill No"} value={billRefNo} onChange={onBillRefNoChange} />
+      </div>
+      <div>
+        <p>二維碼 </p>
+        <select onChange={onExtensionChange} value={fileExt}>
+          <option value="png">PNG</option>
+          <option value="jpeg">JPEG</option>
+          <option value="webp">WEBP</option>
+        </select>
+        <button onClick={onDownloadClick}>下載二維碼 Download</button>
+      </div>
       <div ref={ref} />
       <ul style={listStyles}>
         <li style={docLinkStyle}>
@@ -258,10 +262,6 @@ const IndexPage = () => {
           </li>
         ))}
       </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
     </main>
   )
 }
